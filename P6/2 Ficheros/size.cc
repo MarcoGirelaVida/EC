dfca91a64b550ae7b752de2575b1c5109ca73019
Marco_Girela <marcogirelavida@gmail.com>
2023-12-21 09:43:26 +0100

    P6 added

---
+++ P6/2 Ficheros/size.cc
@@ -0,0 +1,46 @@
+#include <algorithm>    // min_element
+#include <chrono>       // high_resolution_clock
+#include <iomanip>      // setw
+#include <iostream>     // cout
+
+using namespace std::chrono;
+
+const unsigned MINSIZE = 1 << 10; // minimun line size to test:  1KB
+const unsigned MAXSIZE = 1 << 26; // maximun line size to test: 64MB
+const unsigned GAP = 12;          // gap for cout columns
+const unsigned REP = 100;         // number of repetitions of every test
+const unsigned STEPS = 1e6;       // steps
+
+char bytes[MAXSIZE], r = 0;
+
+int main()
+{
+	std::cout << "#" 
+	          << std::setw(GAP - 1) << "line (B)"
+	          << std::setw(GAP + 1) << "time (µs)"
+	          << std::endl;
+
+	for (unsigned size = MINSIZE; size <= MAXSIZE; size *= 2)
+	{
+		duration<double, std::micro> score[REP];
+
+		for (auto &s: score)
+		{
+			auto start = high_resolution_clock::now();
+
+			for (unsigned i = 0; i < STEPS; ++i)
+				// completar aquí...
+				r ^= bytes[(i * 64) % size]; // ... o mejora esto...
+				
+			auto stop = high_resolution_clock::now();
+
+			s = stop - start;
+		}
+
+		std::cout << std::setw(GAP) << size
+		          << std::setw(GAP) << std::fixed << std::setprecision(2)
+		          << std::setw(GAP) << std::min_element(std::begin(score), std::end(score))->count()
+		          << std::endl;
+	}
+}
+
